c9d86f42d84f3b19963e8f1e95afc3371286e154
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-05-27 15:04:13 +0530

    Create datatype_2.py

---
+++ Python_basic/TASK_06/datatype_2.py
@@ -0,0 +1,17 @@
+age=22
+height=5.8
+name="Seelan"
+is_student=True
+subjects=["Mathematics","Science","IT"]
+grades=("A","B","C")
+languages={"Python","Java","C"}
+marks={"mathematics":85,"science":90}
+
+print("Age       :",age,type(age))
+print("Height    :",height,type(height))
+print("Name      :",name,type(name))
+print("Student   :",is_student,type(is_student))
+print("Subjects  :",subjects,type(subjects))
+print("Grades    :",grades,type(grades))
+print("Languages :",languages,type(languages))
+print("Marks     :",marks,type(marks))
