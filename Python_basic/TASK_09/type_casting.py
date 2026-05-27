bc56b55f1fececa5d78ae8b94b14c82d230cc902
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-05-27 15:04:25 +0530

    Create type_casting.py

---
+++ Python_basic/TASK_09/type_casting.py
@@ -0,0 +1,23 @@
+#convert integer to string
+age=22
+print("My age is"+str(age))
+
+#Convert string to integer
+year="2026"
+year_i=int(year)
+print("String to Integer:",year_i)
+
+#Convert string to float
+price="99.99"
+price_f=float(price)
+print("String to Float:",price_f)
+
+#Convert integer to float
+quantity=10
+quantity_f=float(quantity)
+print("Integer to Float:",quantity_f)
+
+#Convert float to Integer
+temperature=36.7
+temp_i=int(temperature)
+print("Float to Integer:",temp_i)
