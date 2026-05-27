edf7a0170a94defd822486d46d3b4990e01ca487
Thulasigan Nagalingam <nstthulasigan@gmail.com>
2026-05-27 15:05:33 +0530

    Create Adding_two_number.py

---
+++ Python_basic/Trash/Adding_two_number.py
@@ -0,0 +1,9 @@
+number1=input("Enter the first number")
+number2=input("Enter the Second number")
+
+num1=int(number1)
+num2=int(number2)
+
+print("First number :",num1)
+print("Second number:",num2)
+print("Total        :",(num1+num2))
